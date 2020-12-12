60715f947308175c1926d797e06f074400e9a553
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2020-12-12 19:40:30 -0300

    [feature] criando tratamento para posição xadrez

---
+++ src/JogoXadrez.Domain/Entidades/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,26 @@
+using JogoXadrez.Domain.Entidades.Tabuleiro;
+
+namespace JogoXadrez.Domain.Entidades.Xadrez
+{
+    public class PosicaoXadrez
+    {
+        public char Coluna { get; private set; }
+        public int Linha { get; private set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.Coluna = coluna;
+            this.Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
