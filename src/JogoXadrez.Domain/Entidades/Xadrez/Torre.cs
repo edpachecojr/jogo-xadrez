23f452bb0370d5b4b71d4d9cc69f20cbe63e0b45
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2020-12-16 14:25:17 -0300

    [feature] movimentacao possivel torre

--- src/JogoXadrez.Domain/Entidades/Xadrez/Torre.cs
+++ src/JogoXadrez.Domain/Entidades/Xadrez/Torre.cs
@@ -13,5 +13,68 @@ namespace JogoXadrez.Domain.Entidades.Xadrez
         {
             return "T";
         }
+
+        private bool PodeMover(Posicao posicao)
+        {
+            Peca peca = Tabuleiro.GetPeca(posicao);
+            return peca == null || peca.Cor != this.Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] MatrizMovimentosPossiveis = new bool[this.Tabuleiro.Linhas, this.Tabuleiro.Colunas];
+
+            Posicao posicao = new Posicao(0, 0);
+
+            // acima
+            posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            while (this.Tabuleiro.PosicaoValida(posicao) && this.PodeMover(posicao))
+            {
+                MatrizMovimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.GetPeca(posicao) != null && Tabuleiro.GetPeca(posicao).Cor != this.Cor)
+                {
+                    break;
+                }
+                posicao.DefinirValores(posicao.Linha - 1, posicao.Coluna);
+            }
+
+            // abaixo
+            posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            while (this.Tabuleiro.PosicaoValida(posicao) && this.PodeMover(posicao))
+            {
+                MatrizMovimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.GetPeca(posicao) != null && Tabuleiro.GetPeca(posicao).Cor != this.Cor)
+                {
+                    break;
+                }
+                posicao.DefinirValores(posicao.Linha + 1, posicao.Coluna);
+            }
+
+            // direita
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            while (this.Tabuleiro.PosicaoValida(posicao) && this.PodeMover(posicao))
+            {
+                MatrizMovimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.GetPeca(posicao) != null && Tabuleiro.GetPeca(posicao).Cor != this.Cor)
+                {
+                    break;
+                }
+                posicao.DefinirValores(posicao.Linha, posicao.Coluna + 1);
+            }
+
+            // esquerda
+            posicao.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            while (this.Tabuleiro.PosicaoValida(posicao) && this.PodeMover(posicao))
+            {
+                MatrizMovimentosPossiveis[posicao.Linha, posicao.Coluna] = true;
+                if (Tabuleiro.GetPeca(posicao) != null && Tabuleiro.GetPeca(posicao).Cor != this.Cor)
+                {
+                    break;
+                }
+                posicao.DefinirValores(posicao.Linha, posicao.Coluna - 1);
+            }
+
+            return MatrizMovimentosPossiveis;
+        }
     }
 }
