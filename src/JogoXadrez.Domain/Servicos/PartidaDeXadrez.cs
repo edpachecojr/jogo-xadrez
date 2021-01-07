15d59bf9217a3984b45531d0adbb78e1c911ba0d
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2021-01-07 19:58:58 -0300

    [feature] acrescentando todas as peças ao tabuleiro

--- src/JogoXadrez.Domain/Servicos/PartidaDeXadrez.cs
+++ src/JogoXadrez.Domain/Servicos/PartidaDeXadrez.cs
@@ -155,26 +155,40 @@ namespace JogoXadrez.Domain.Servicos
         }
         private void ColocarPecas()
         {
-            // this.ColocarNovaPeca('c', 1, new Torre(this._tabuleiro, CorEnum.Branca));
-            // this.ColocarNovaPeca('c', 2, new Torre(this._tabuleiro, CorEnum.Branca));
-            // this.ColocarNovaPeca('d', 2, new Torre(this._tabuleiro, CorEnum.Branca));
-            // this.ColocarNovaPeca('e', 2, new Torre(this._tabuleiro, CorEnum.Branca));
-            // this.ColocarNovaPeca('e', 1, new Torre(this._tabuleiro, CorEnum.Branca));
-            // this.ColocarNovaPeca('d', 1, new Rei(this._tabuleiro, CorEnum.Branca));
 
-            // this.ColocarNovaPeca('c', 7, new Torre(this._tabuleiro, CorEnum.Preta));
-            // this.ColocarNovaPeca('c', 8, new Torre(this._tabuleiro, CorEnum.Preta));
-            // this.ColocarNovaPeca('d', 7, new Torre(this._tabuleiro, CorEnum.Preta));
-            // this.ColocarNovaPeca('e', 7, new Torre(this._tabuleiro, CorEnum.Preta));
-            // this.ColocarNovaPeca('e', 8, new Torre(this._tabuleiro, CorEnum.Preta));
-            // this.ColocarNovaPeca('d', 8, new Rei(this._tabuleiro, CorEnum.Preta));
-            this.ColocarNovaPeca('c', 1, new Torre(this._tabuleiro, CorEnum.Branca));
-            this.ColocarNovaPeca('d', 1, new Rei(this._tabuleiro, CorEnum.Branca));
-            this.ColocarNovaPeca('h', 7, new Torre(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('a', 1, new Torre(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('b', 1, new Cavalo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('c', 1, new Bispo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('d', 1, new Dama(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('e', 1, new Rei(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('f', 1, new Bispo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('g', 1, new Cavalo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('h', 1, new Torre(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('a', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('b', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('c', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('d', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('e', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('f', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('g', 2, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('h', 2, new Peao(this._tabuleiro, CorEnum.Branca));
 
-            this.ColocarNovaPeca('a', 8, new Rei(this._tabuleiro, CorEnum.Preta));
-            this.ColocarNovaPeca('b', 8, new Torre(this._tabuleiro, CorEnum.Preta));
-            this.ColocarNovaPeca('d', 8, new Bispo(this._tabuleiro, CorEnum.Preta));
+            this.ColocarNovaPeca('a', 8, new Torre(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('b', 8, new Cavalo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('c', 8, new Bispo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('d', 8, new Dama(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('e', 8, new Rei(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('f', 8, new Bispo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('g', 8, new Cavalo(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('h', 8, new Torre(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('a', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('b', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('c', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('d', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('e', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('f', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('g', 7, new Peao(this._tabuleiro, CorEnum.Branca));
+            this.ColocarNovaPeca('h', 7, new Peao(this._tabuleiro, CorEnum.Branca));
         }
 
         public Tabuleiro Tabuleiro
